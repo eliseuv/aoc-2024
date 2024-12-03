151cfb36e8fc3ddde7c31099ec673362d942ab93
evf <eliseuv816@gmail.com>
2024-12-03 15:58:02 -0300

    Python template file

---
+++ python/_template.py
@@ -0,0 +1,20 @@
+def part1(input):
+    pass
+
+
+def part2(input):
+    pass
+
+
+def main():
+    # Load input
+    with open("../inputs/day-xx.txt", "r") as file:
+        input = file.readlines()
+    print("Input data:\n", input)
+
+    print("Part 1:", part1(input))
+    print("Part 2:", part2(input))
+
+
+if __name__ == "__main__":
+    main()
