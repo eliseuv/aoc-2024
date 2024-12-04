66aa64ee699f7349948ae3197536fd4545b2d740
evf <eliseuv816@gmail.com>
2024-12-04 17:29:38 -0300

    Day 1 in OCaml

--- ocaml/bin/day01.ml
+++ ocaml/bin/day01.ml
@@ -1,13 +1,27 @@
-open Base
+let () = print_endline "Advent of Code 2024 day 1"
 
-let part1 lines =
+let part1 (list_left, list_right) =
+  let sorted_left = List.sort compare list_left in
+  let sorted_right = List.sort compare list_right in
+  List.fold_left
+    (fun sum x -> sum + x)
+    0
+    (List.map
+       (fun (x, y) -> abs (x - y))
+       (List.combine sorted_left sorted_right) )
 
+let part2 (list_left, list_right) =
+  let occur_left = Aoc2024.Utils.count_occurences list_left in
+  List.fold_left
+    (fun sum (value, count) ->
+      sum
+      + (count * value * Aoc2024.Utils.count_value_occurences value list_right)
+      )
+    0 occur_left
 
-let solve filename =
-    let lines = Aoc2024.Utils.read_lines filename in
-    Stdio.printf "Part 1: %d\n" @@ part1 lines
-       (* Stdio.printf "Part 2: %d\n" @@ part2 lines;; *)
+let solve input_filename =
+  let lists = List.split (Aoc2024.Utils.read_list_of_pairs input_filename) in
+  Stdio.printf "Part 1: Total distance = %d\n" @@ part1 lists ;
+  Stdio.printf "Part 2: Similarity score = %d\n" @@ part2 lists
 
-
-let () =
-    solve "../inpts/day-01.txt"
+let () = solve "../inputs/day-01.txt"
--- ocaml/lib/utils.ml
+++ ocaml/lib/utils.ml
@@ -1,6 +1,25 @@
-open Base
+(* Read list of integer pairs separated by whitespace from datafile *)
+let read_list_of_pairs filename =
+  let ic = Stdio.In_channel.create filename in
+  let rec read_lines acc_list =
+    try
+      let line = input_line ic in
+      let parts = Str.split (Str.regexp "[ \t]+") line in
+      match parts with
+      | [a; b] ->
+          read_lines ((int_of_string a, int_of_string b) :: acc_list)
+      | _ ->
+          read_lines acc_list
+    with End_of_file -> Stdio.In_channel.close ic ; List.rev acc_list
+  in
+  read_lines []
 
-let read_lines file =
-    Stdio.In_channel.with_file file ~f:(fun ch ->
-        let x = In_channel.input_all ch in
-        String.split_lines x)
+(* Count the number of occurences of a given value in a list *)
+let count_value_occurences value list =
+  List.fold_left (fun acc x -> acc + if x == value then 1 else 0) 0 list
+
+(* Returns a list of pairs containing at the first position a value and at the second the number of times this values occurs in the given list *)
+let count_occurences list =
+  let uniq = List.sort_uniq compare list in
+  let counts = List.map (fun value -> count_value_occurences value list) uniq in
+  List.combine uniq counts
