fce1baa812d0c24a66c63b118949b81d44f33498
evf <eliseuv816@gmail.com>
2024-12-07 21:48:34 -0300

    Day 2 in Haskell

--- haskell/lib/Day02.hs
+++ haskell/lib/Day02.hs
@@ -1,13 +1,27 @@
 module Day02 where
 
-import MyLib (readMatrix)
+import Foreign (fromBool)
+import MyLib (allEqual, diff, inRange, listRemover, readMatrix)
+import Text.Printf (printf)
 
--- isSafe report =
---     let dx = diff report
---      in dx
+isSafe :: (Ord a, Num a) => [a] -> Bool
+isSafe report =
+    let dx_report = diff report
+     in and (allEqual (map signum dx_report) : map (inRange (1, 3) . abs) dx_report)
+
+isSafeDampened :: (Ord a, Num a) => [a] -> Bool
+isSafeDampened report = or (isSafe report : map isSafe dampenedCandidates)
+  where
+    dampenedCandidates = listRemover report
+
+part1 :: (Ord a, Num a) => [[a]] -> Int
+part1 = sum . map (fromBool . isSafe)
+
+part2 :: (Ord a, Num a) => [[a]] -> Int
+part2 = sum . map (fromBool . isSafeDampened)
 
 inputPath :: FilePath
-inputPath = "../inputs/day-02-test.txt"
+inputPath = "../inputs/day-02.txt"
 
 solution :: IO ()
 solution =
@@ -15,4 +29,5 @@ solution =
         putStrLn "Day 2"
         contents <- readFile inputPath
         let reports = readMatrix contents :: [[Int]]
-        print reports
+        printf "Part 1: Number of safe reports = %d\n" $ part1 reports
+        printf "Part 2: Number of safe reports considering dampening = %d\n" $ part2 reports
--- haskell/lib/MyLib.hs
+++ haskell/lib/MyLib.hs
@@ -23,3 +23,33 @@ countOccurences x = length . filter (== x)
 -- Counts the number of occurrences of each unique element in a list
 getCounts :: (Ord a) => [a] -> [(a, Int)]
 getCounts = map (\lst -> (head lst, length lst)) . group . sort
+
+-- Similar to `numpy.diff`
+diff :: (Num a) => [a] -> [a]
+diff =
+    let
+        helper :: (Num a) => [a] -> [a] -> [a]
+        helper acc [] = acc
+        helper acc [_] = acc
+        helper acc (y : x : xs) = helper ((y - x) : acc) (x : xs)
+     in
+        helper []
+
+-- Checks if all elements in a list are equal to each other
+allEqual :: (Eq a) => [a] -> Bool
+allEqual lst =
+    all (== head lst) (tail lst)
+
+-- Checks if all elements of a list are between a range given by a value pair
+inRange :: (Ord a) => (a, a) -> a -> Bool
+inRange (a, b) x =
+    (a <= x) && (x <= b)
+
+-- Generates copy of a given list each copy having a different element from the original list removed
+listRemover :: [a] -> [[a]]
+listRemover list =
+    let
+        remover acc _ [] = acc
+        remover acc before (x : xs) = remover ((reverse before ++ xs) : acc) (x : before) xs
+     in
+        remover [] [] list
